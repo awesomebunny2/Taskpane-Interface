d808ad2e1e0b7ff2f35d34b4e58791f500baa09d
Matthew Calvert <mattc@themailshark.com>
2022-03-15 18:09:54 -0400

    just about ready

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ $(function() {
 });
 
 $("#subject").on("keyup", function() {
-  
+
     var paste = $(this).val(); // Get value from pasted input
    
     if (paste.length == 0) { // If what's pasted is empty
@@ -36,21 +36,6 @@ $("#subject").on("keyup", function() {
 
         // Split at "-"s
         var splitPaste = paste.split("-");
-
-        /**
-         client: splitPaste[0]
-        product: splitPaste[1]
-        codes: splitPaste[2]
-        */
-
-        //OR
-
-        /*
-        client: splitPaste[0]
-        location: splitPaste[1]
-        product: splitPaste[2]
-        codes: splitPaste[3]
-        */
 
         var blanks = splitPaste.includes("");
 
@@ -85,17 +70,29 @@ $("#subject").on("keyup", function() {
 
         };
 
+        var plasticS = removeFirstAndLastSpace(noBlanksArr[noBlanksArr.length - 2]);
+
+        if (plasticS == "S" || plasticS == "Flat") {
+
+            var plasticSIndex = noBlanksArr.indexOf(noBlanksArr[noBlanksArr.length - 2]);
+
+            noBlanksArr.splice(plasticSIndex, 1);
+
+            if (plasticS == "Flat") {
+
+                var productPostFlatIndex = noBlanksArr.indexOf(noBlanksArr[noBlanksArr.length - 2]);
+
+                noBlanksArr[productPostFlatIndex] = noBlanksArr[noBlanksArr.length - 2] + "Flat";
+
+            };
+
+        };
+
         if (noBlanksArr.length > 3) { //if the subject line includes a location, do this...
 
             // .NET stuff at end (~/*20104,51824,2*/~)
             // Remove spaces (just in case), "~/*", "*/~", then split at ","
             var splitCodes = noBlanksArr[noBlanksArr.length - 1].replace(' ','').replace('~/*','').replace('*/~','').split(",");
-
-            /**
-             client id: splitCodes[0]
-            contract: splitCodes[1]
-            line: splitCodes[2]
-            */
 
             var theClient = noBlanksArr[0];
             var updatedClient = removeFirstAndLastSpace(theClient);
@@ -105,6 +102,7 @@ $("#subject").on("keyup", function() {
 
             var theProduct = noBlanksArr[noBlanksArr.length - 2];
             var updatedProduct = removeFirstAndLastSpace(theProduct);
+            updatedProduct = productAdjust(updatedProduct);
 
             var theCode = splitCodes[0];
             var updatedCode = removeFirstAndLastSpace(theCode);
@@ -116,7 +114,7 @@ $("#subject").on("keyup", function() {
             try {
                 $("#client").val(updatedClient);
                 $("#location").val(updatedLocation);
-                $("#product").val(updatedProduct);
+                $("#product").val(updatedProduct).removeClass("grey-sel");
                 $("#code").val(updatedCode);
             } catch (e) {
                 // Something was wrong with the subject
@@ -129,17 +127,12 @@ $("#subject").on("keyup", function() {
             // Remove spaces (just in case), "~/*", "*/~", then split at ","
             var splitCodes = noBlanksArr[2].replace(' ','').replace('~/*','').replace('*/~','').split(",");
 
-            /**
-             client id: splitCodes[0]
-            contract: splitCodes[1]
-            line: splitCodes[2]
-            */
-
             var theClient = noBlanksArr[0];
             var updatedClient = removeFirstAndLastSpace(theClient);
 
             var theProduct = noBlanksArr[1];
             var updatedProduct = removeFirstAndLastSpace(theProduct);
+            updatedProduct = productAdjust(updatedProduct);
 
             var theCode = splitCodes[0];
             var updatedCode = removeFirstAndLastSpace(theCode);
@@ -149,7 +142,7 @@ $("#subject").on("keyup", function() {
             // if we can't fill the data out, error:
             try {
                 $("#client").val(updatedClient);
-                $("#product").val(updatedProduct);
+                $("#product").val(updatedProduct).removeClass("grey-sel");
                 $("#code").val(updatedCode);
             } catch (e) {
                 // Something was wrong with the subject
@@ -178,22 +171,181 @@ function removeFirstAndLastSpace(splitItem) {
     return splitItem;
 };
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-// function myFunction() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-//   }
-  
-//   // Close the dropdown menu if the user clicks outside of it
-//   window.onclick = function(event) {
-//     if (!event.target.matches('.dropbtn')) {
-//       var dropdowns = document.getElementsByClassName("dropdown-content");
-//       var i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         var openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         }
-//       }
-//     }
-//   }
+function productAdjust(leProductInput) {
+
+    var leProduct;
+
+    if ($.inArray(leProductInput, ["EDDM MENU", "MENU", "MenuFlat"]) >= 0) {
+        leProduct = "Menu";
+
+      } else if ($.inArray(leProductInput, ["EDDM XL MENU", "MenuXL", "MenuXLFlat"]) >= 0) {
+        leProduct = "MenuXL";
+
+      } else if ($.inArray(leProductInput, ["Menu Small"]) >= 0) {
+        leProduct = "Small Menu";
+
+      } else if ($.inArray(leProductInput, ["Brochure 10.5x17", "EDDMBroch"]) >= 0) {
+        leProduct = "Brochure";
+
+      } else if ($.inArray(leProductInput, ["BrochureXL", "EDDMBrochXL"]) >= 0) {
+        leProduct = "BrochureXL";
+
+      } else if ($.inArray(leProductInput, ["Brochure Small"]) >= 0) {
+        leProduct = "Small Brochure";
+
+      } else if ($.inArray(leProductInput, ["100# Gloss Postcard", "EDDM POSTCARD", "Long Postcard", "POSTCARD"]) >= 0) {
+        leProduct = "Postcard";
+
+      } else if ($.inArray(leProductInput, ["EDDMJumboPC", "JUMBOPC"]) >= 0) {
+        leProduct = "Jumbo Postcard";
+
+      } else if ($.inArray(leProductInput, ["ColossalPC", "EDDMColossal"]) >= 0) {
+        leProduct = "Colossal Postcard";
+
+      } else if ($.inArray(leProductInput, ["EDDM Scratch Off", "SCRATCHOFF"]) >= 0) {
+        leProduct = "Scratch-Off Postcard";
+
+      } else if ($.inArray(leProductInput, ["EDDMJumboSO", "Jumbo Scratch"]) >= 0) {
+        leProduct = "Jumbo Scratch-Off Postcard";
+
+      } else if ($.inArray(leProductInput, ["EDDMPeelAGift", "EDDMPizzaPeelCard", "Peel A Gift", "PizzaPeelCard"]) >= 0) {
+        leProduct = "Peel-A-Box Postcard";
+
+      } else if ($.inArray(leProductInput, ["EDDM Mag", "MAGNET"]) >= 0) {
+        leProduct = "Magnet";
+
+      } else if ($.inArray(leProductInput, ["EDDM Folded Magnet", "Folded Magnet"]) >= 0) {
+        leProduct = "Folded Magnet";
+
+      } else if ($.inArray(leProductInput, ["2 sided box topper", "2 Sided Flyer"]) >= 0) {
+        leProduct = "2SBT";
+
+      } else if ($.inArray(leProductInput, ["Box Topper January", "Box Topper February", "Box Topper March", "Box Topper April", "Box Topper May", "Box Topper June", "Box Topper July", "Box Topper August", "Box Topper September", "Box Topper October", "Box Topper November", "Box Topper December"]) >= 0) {
+        leProduct = "Box Topper";
+
+      } else if ($.inArray(leProductInput, ["80LBFLYER", "Flyer 8.5X11"]) >= 0) {
+        leProduct = "Flyer";
+
+      } else if ($.inArray(leProductInput, ["DOOR HANGER 100LB"]) >= 0) {
+        leProduct = "Door Hanger";
+
+      } else if ($.inArray(leProductInput, ["Plastic PC Sm"]) >= 0) {
+        leProduct = "Small Plastic";
+
+      } else if ($.inArray(leProductInput, ["Plastic PC Med"]) >= 0) {
+        leProduct = "Medium Plastic";
+
+      } else if ($.inArray(leProductInput, ["Plastic PC Lg"]) >= 0) {
+        leProduct = "Large Plastic";
+
+      } else if ($.inArray(leProductInput, ["COUPON BOOK"]) >= 0) {
+        leProduct = "Coupon Booklet";
+
+      } else if ($.inArray(leProductInput, ["CustomEnv", "Env #10 8.5x11 S1", "Env #10 8.5x11 S2", "Env #10 8.5x11 V1", "Env #10 8.5x11 V2"]) >= 0) {
+        leProduct = "Envelope Mailer";
+
+      } else if ($.inArray(leProductInput, ["BirthdayPC"]) >= 0) {
+        leProduct = "Birthday Postcard";
+
+      } else if ($.inArray(leProductInput, ["NEW MOVERS POSTCARD"]) >= 0) {
+        leProduct = "New Mover";
+
+      } else if ($.inArray(leProductInput, ["NEW MOVERS PLASTIC"]) >= 0) {
+        leProduct = "Plastic New Mover";
+
+      } else if ($.inArray(leProductInput, ["WCCust", "WC2030", "WC2430", "WC2436", "WC3040"]) >= 0) {
+        leProduct = "Window Clings";
+
+      } else if ($.inArray(leProductInput, ["BusinessCard"]) >= 0) {
+        leProduct = "Business Cards";
+
+      } else if ($.inArray(leProductInput, ["Artwork Only"]) >= 0) {
+        leProduct = "Artwork Only";
+
+      } else if ($.inArray(leProductInput, ["Env #10 8.5x14 S1", "Env #10 8.5x14 S2", "Env #10 8.5x14 V1", "Env #10 8.5x14 V2"]) >= 0) {
+        leProduct = "Legal Letter";
+
+      } else if ($.inArray(leProductInput, ["Custom Insert"]) >= 0) {
+        leProduct = "Envelope Insert";
+
+      } else if ($.inArray(leProductInput, ["MENU XXL"]) >= 0) {
+        leProduct = "MenuXXL";
+
+    } else if ($.inArray(leProductInput, ["MPBTAC"]) >= 0) {
+        leProduct = "Marco's 6.8 Box Topper A La Carte - MPBTAC";
+
+    } else if ($.inArray(leProductInput, ["MPBTNO"]) >= 0) {
+        leProduct = "Marco's 6.8 Box Topper National Offer - MPBTNO";
+
+    } else if ($.inArray(leProductInput, ["MPP"]) >= 0) { 
+        leProduct = "Marco's 8.5x11 Poster - MPP";
+
+    } else if ($.inArray(leProductInput, ["MPPO2436"]) >= 0) { 
+        leProduct = "Marco's 24x36 Poster - MPPO2436";
+
+    } else if ($.inArray(leProductInput, ["MPPO3040"]) >= 0) { 
+        leProduct = "Marco's 30x40 Poster - MPPO3040";
+
+    } else if ($.inArray(leProductInput, ["MPCC"]) >= 0) { 
+        leProduct = "Marco's Counter Card - MPCC";
+
+    } else if ($.inArray(leProductInput, ["MPCMS"]) >= 0) { 
+        leProduct = "Marco's Door Stickers - MPCMS";
+
+    } else if ($.inArray(leProductInput, ["MPIS"]) >= 0) { 
+        leProduct = "Marco's Interior Sticker - MPIS";
+
+    } else if ($.inArray(leProductInput, ["MPSCFS"]) >= 0) { 
+        leProduct = "Marco's Floor Stickers - MPSCFS";
+
+    } else if ($.inArray(leProductInput, ["MPND"]) >= 0) { 
+        leProduct = "Marco's Napkin Dispenser Insert - MPND";
+
+    } else if ($.inArray(leProductInput, ["MPPICMENU"]) >= 0) { 
+        leProduct = "Marco's Laminated Menu - MPPICMENU";
+
+    } else if ($.inArray(leProductInput, ["MPCVDLAM"]) >= 0) { 
+        leProduct = "Marco's Laminated Sign - MPCVDLAM";
+
+    } else if ($.inArray(leProductInput, ["MPC"]) >= 0) { 
+        leProduct = "Marco's 8.5x11 Window Cling - MPC";
+
+    } else if ($.inArray(leProductInput, ["MPEXTWC2436AC"]) >= 0) { 
+        leProduct = "Marco's 24x36 Exterior Window Cling A La Carte - MPEXTWC2436AC";
+
+    } else if ($.inArray(leProductInput, ["MPEXTWC2436NO"]) >= 0) { 
+        leProduct = "Marco's 24x36 Exterior Window Cling National Offer - MPEXTWC2436NO";
+
+    } else if ($.inArray(leProductInput, ["MPEXTWC3040AC"]) >= 0) { 
+        leProduct = "Marco's 30x40 Exterior Window Cling A La Carte - MPEXTWC3040AC";
+
+    } else if ($.inArray(leProductInput, ["MPEXTWC3040NO"]) >= 0) { 
+        leProduct = "Marco's 30x40 Exterior Window Cling National Offer - MPEXTWC3040NO";
+
+    } else if ($.inArray(leProductInput, ["MPINTWC2436AC"]) >= 0) { 
+        leProduct = "Marco's 24x36 Interior Window Cling A La Carte - MPINTWC2436AC";
+
+    } else if ($.inArray(leProductInput, ["MPINTWC2436NO"]) >= 0) { 
+        leProduct = "Marco's 24x36 Interior Window Cling National Offer - MPINTWC2436NO";
+
+    } else if ($.inArray(leProductInput, ["MPINTWC3040AC"]) >= 0) { 
+        leProduct = "Marco's 30x40 Interior Window Cling A La Carte - MPINTWC3040AC";
+
+    } else if ($.inArray(leProductInput, ["MPINTWC3040NO"]) >= 0) { 
+        leProduct = "Marco's 30x40 Interior Window Cling National Offer - MPINTWC3040NO";
+
+    } else if ($.inArray(leProductInput, ["MPNutGuide"]) >= 0) { 
+        leProduct = "Marco's Nutritional Guide - MPNutGuide";
+
+    } else if ($.inArray(leProductInput, ["Custom100", "CUSTOM80", "CustomSilk", "Other"]) >= 0) { 
+        leProduct = "Other";
+
+    } else {
+    leProduct = "";
+    };
+
+    return leProduct;
+
+};
+
+
